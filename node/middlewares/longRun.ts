bea902f61816c13d8c8e0a9119386358e96ce3e4
tiagonapoli <tiago.napoli@vtex.com.br>
2019-10-10 12:28:26 -0300

    Add logs

--- node/middlewares/longRun.ts
+++ node/middlewares/longRun.ts
@@ -1,4 +1,5 @@
 export async function longRun(ctx: Context) {
+    console.log("Started running longRun")
     ctx.set('Cache-Control', 'no-cache')
     await new Promise((resolve) => {
       setTimeout(resolve, 7000)
@@ -6,5 +7,6 @@ export async function longRun(ctx: Context) {
     
     ctx.response.status = 200
     ctx.response.body = 'Hello, IO!'  
+    console.log("Finished running longRung")
 }
   
