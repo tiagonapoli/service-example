07fbf3ea6c1a55a8e5edc69fd94e10fecc6bfa6d
tiagonapoli <tiago.napoli@vtex.com.br>
2020-03-04 17:22:28 -0300

    asdf

--- node/index.ts
+++ node/index.ts
@@ -1,12 +1,12 @@
 import { a } from "@tiagonapoli/test-pkg-a";
 import { b } from "@tiagonapoli/test-pkg-b";
 import { Service, ServiceContext } from "@vtex/api";
-import { initServiceTracer } from "./tracing";
+// import { initServiceTracer } from "./tracing";
 
 console.log("IMPORT DE A", a);
 console.log("IMPORT DE B", b);
 
-const tracer = initServiceTracer()
+// const tracer = initServiceTracer();
 
 // const socket = dgram.createSocket('udp4')
 
@@ -14,14 +14,25 @@ const tracer = initServiceTracer()
 //   console.log(err, sent)
 // })
 
+function randomIntFromInterval(min: number, max: number) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const sleep = (ms: number) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+};
 
 export default new Service({
   routes: {
-    test: (ctx: ServiceContext) => {
-      const span = tracer.startSpan('test')
+    test: async (ctx: ServiceContext) => {
+      // const span = tracer.startSpan("test");
+      await sleep(randomIntFromInterval(1000, 3000))
       ctx.body = "test";
       ctx.status = 200;
-      span.finish()
+      // span.finish();
     }
   }
 });
--- node/tracing.ts
+++ node/tracing.ts
@@ -1,41 +1,54 @@
 import {
-    initTracer as initJaegerTracer,
-    TracingConfig,
-    TracingOptions,
-  } from 'jaeger-client'
-  
-  const initTracer = (
-    serviceName: string,
-    defaultTags: Record<string, string | boolean>
-  ) => {
-    const config: TracingConfig = {
-      serviceName,
-      sampler: {
-        type: 'const',
-        param: 1,
-      },
-      reporter: {
-        logSpans: true,
-        agentHost: process.env.VTEX_OWN_NODE_IP
-      },
+  APP,
+  LINKED,
+  PRODUCTION,
+  REGION,
+  WORKSPACE
+} from "@vtex/api/lib/constants";
+import {
+  initTracer as initJaegerTracer,
+  TracingConfig,
+  TracingOptions
+} from "jaeger-client";
+
+const initTracer = (
+  serviceName: string,
+  defaultTags: Record<string, string | boolean>
+) => {
+  const config: TracingConfig = {
+    serviceName,
+    sampler: {
+      type: "const",
+      param: 1
+    },
+    reporter: {
+      logSpans: true,
+      agentHost: "10.42.133.197"
     }
-  
-    const options: TracingOptions = {
-      tags: defaultTags,
-      logger: {
-        info(msg) {
-          console.log('INFO ', msg)
-        },
-        error(msg) {
-          console.log('ERROR', msg)
-        },
+  };
+
+  const options: TracingOptions = {
+    tags: defaultTags,
+    logger: {
+      info(msg) {
+        console.log("INFO ", msg);
       },
+      error(msg) {
+        console.log("ERROR", msg);
+      }
     }
-  
-    return initJaegerTracer(config, options)
-  }
-  
-  export const initServiceTracer = () => {
-    return initTracer('disgraca', {})
-  }
-  
+  };
+
+  return initJaegerTracer(config, options);
+};
+
+export const initServiceTracer = () => {
+  return initTracer("test1", {
+    version: APP.VERSION,
+    vendor: APP.VENDOR,
+    region: REGION,
+    linked: LINKED,
+    production: PRODUCTION,
+    worskpace: WORKSPACE
+  });
+};
